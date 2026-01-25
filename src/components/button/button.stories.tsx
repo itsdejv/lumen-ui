38e463d0c1de86f4a38aac501f252ccadcead8b5
David Rihacek <itssdejv@gmail.com>
2026-01-25 22:55:33 +0100

    button WIP

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -2,11 +2,40 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { Button } from "./button";
 
 const meta = {
-    title: "Example/Button",
-    component: Button,
-    tags: ["docsPage"],
-    argTypes: {
+  title: "Example/Button",
+  component: Button,
+  tags: ["docsPage"],
+  argTypes: {
+    children: {
+      control: "text",
     },
+    asChild: {
+      control: "boolean",
+    },
+    intent: {
+      control: "select",
+      options: ["primary", "secondary"],
+    },
+    variant: {
+      control: "select",
+      options: ["solid", "outline", "ghost"],
+    },
+    size: {
+      control: "select",
+      options: ["m"],
+    },
+    animation: {
+      control: "select",
+      options: ["none", "scale", "ripple", "jump"],
+    },
+  },
+  args: {
+    children: "Button",
+    intent: "primary",
+    variant: "solid",
+    size: "m",
+    animation: "scale",
+  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,3 +1,71 @@
-export const Button = () => {
-  return <div className="text-2xl border border-yellow-200">NEW Button</div>;
+import { ButtonHTMLAttributes } from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "../../utils/utils.ts";
+
+const buttonVariants = cva(
+  cn(
+    "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm",
+    "font-medium transition-all cursor-pointer",
+  ),
+  {
+    variants: {
+      variant: {
+        solid: "",
+        outline: "",
+        ghost: "",
+      },
+      size: {
+        m: "h-9 px-4 py-2 has-[>svg]:px-3",
+      },
+      intent: {
+        primary: "",
+        secondary: "",
+      },
+      animation: {
+        none: "",
+        scale: "active:scale-95",
+        jump: "active:translate-y-[3px] duration-[50ms]",
+        ripple: "relative overflow-hidden",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "solid",
+        intent: "primary",
+        className: "bg-primary text-primary-foreground",
+      },
+      {
+        variant: "solid",
+        intent: "secondary",
+        className: "bg-secondary text-secondary-foreground",
+      },
+    ],
+    defaultVariants: {
+      variant: "solid",
+      size: "m",
+      intent: "primary",
+      animation: "scale",
+    },
+  },
+);
+
+interface ButtonProps
+  extends
+    ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+export const Button = (props: ButtonProps) => {
+  const { asChild, size, animation, variant, intent, ...restProps } = props;
+
+  const Comp = asChild ? Slot : "button";
+
+  return (
+    <Comp
+      className={cn(buttonVariants({ size, animation, variant, intent }))}
+      {...restProps}
+    />
+  );
 };
